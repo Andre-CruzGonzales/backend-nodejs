3770c4e440607d9e68e6ca1b5e527f97df87ac80
Andre Cruz <ajcruzg95@gmail.com>
2021-12-07 08:15:42 -0500

    -

--- src/routes/Categoria.js
+++ src/routes/Categoria.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const categoriaSchema = require("../models/Categoria");
 //creacion de articulos
+
 /**
  * @swagger
  * components:
